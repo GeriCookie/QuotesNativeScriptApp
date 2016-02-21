9a448df51bc9637339db842fe12c6f8f2017313b
GeriCookie <Gergana.Bailova@gmail.com>
2016-02-21 21:45:31 +0200

    Bug fixed

---
+++ Routes/myQuotesRoutes.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    passport = require('passport');
+
+var auth = function() {
+  return passport.authenticate('bearer', {
+    session: false
+  });
+};
+
+var routes = function(User, Quote) {
+  var myQuotesRouter = express.Router();
+
+  var myQuotesController = require('../Controllers/myQuotesController')(User, Quote);
+
+  quoteRouter.get('/',auth(), quoteController.get);
+
+  return myQuotesRouter;
+};
+module.exports = routes;
